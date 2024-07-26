aa1e07a1d1ba55a46ad250ce12a4872191b16178
mertncu <ymuncu@gmail.com>
2024-07-26 22:56:52 +0300

    Api Repository Added

---
+++ app/src/main/java/com/eosdev/yemenu/data/repository/ApiService.kt
@@ -0,0 +1,14 @@
+package com.eosdev.yemenu.data.repository
+
+import retrofit2.Response
+import retrofit2.http.Body
+
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.DELETE
+
+interface ApiService {
+
+}
