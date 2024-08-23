326dfcf61b3caa8d54437f273f9de6770cd4f582
mertncu <ymuncu@gmail.com>
2024-08-23 04:52:30 +0300

    Add .env file and configure RetrofitClient with environment variables

---
+++ app/src/main/java/com/eosdev/yemenu/data/retrofit/RetrofitClient.kt
@@ -0,0 +1,21 @@
+package com.eosdev.yemenu.data.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import io.github.cdimascio.dotenv.dotenv
+
+
+object RetrofitClient {
+    private val dotenv = dotenv {
+        directory = "./"
+    }
+
+    private val BASE_URL = dotenv["BASE_URL"]
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
